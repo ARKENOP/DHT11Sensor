3357c449669fed540f5d376b5b355fd76839ccaa
Paul SUPIOT <psupiot44@gmail.com>
2024-04-08 11:41:24 +0200

    mise en place de l'envoie des données dans Google Sheet avec make.com

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,68 @@
 #include <Arduino.h>
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
 #include <WiFi.h>
 #include <HTTPClient.h>
 
 #define DHTPIN 21
-
 #define DHTTYPE DHT11
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
 
-const char* ssid = "YOUR_SSID";
-const char* password = "YOUR_PASSWORD";
-const char* url = "ggtizg0u4y2el4bzaw9lc5wr68mf93e5";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* url = "https://hook.eu2.make.com/id";
 
 unsigned long previousMillis = 0;
-const long interval = 1800000;
+const long interval = 600000; // 30 minutes
+
+void sendData(float temperature, float humidity);
 
 void setup() {
   Serial.begin(9600);
-  // Initialize DHT sensor
   dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
-  // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   Serial.println(F("------------------------------------"));
   Serial.println(F("Temperature Sensor"));
   Serial.print(F("Sensor:       ")); Serial.println(sensor.name);
   Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
   Serial.println(F("Humidity Sensor"));
   Serial.print(F("Sensor:       ")); Serial.println(sensor.name);
   Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
   unsigned long currentMillis = millis();
+
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-  }
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-    sendData(event.temperature, event.relative_humidity); // Send data to google sheet
+    sensors_event_t temp_event, humidity_event;
+    dht.temperature().getEvent(&temp_event);
+    if (isnan(temp_event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      Serial.print(F("Temperature: "));
+      Serial.print(temp_event.temperature);
+      Serial.println(F("°C"));
+    }
+    dht.humidity().getEvent(&humidity_event);
+    if (isnan(humidity_event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      Serial.print(humidity_event.relative_humidity);
+      Serial.println(F("%"));
+      sendData(temp_event.temperature, humidity_event.relative_humidity); // Envoyer les données à Google Sheets
+    }
   }
 }
 
@@ -80,7 +75,7 @@ void sendData(float temperature, float humidity) {
   Serial.println("Connected to WiFi");
 
   HTTPClient http;
-  String urlWithParams = url + "?temp=" + String(temperature, 2) + "&humi=" + String(humidity, 2);
+  String urlWithParams = String(url) + "?temp=" + String(temperature, 2) + "&humi=" + String(humidity, 2);
   http.begin(urlWithParams);
 
   int httpResponseCode = http.GET();
@@ -94,4 +89,3 @@ void sendData(float temperature, float humidity) {
 
   http.end();
 }
-
