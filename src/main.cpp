0e928bb1160047b1e3daf0a18d9239e0ba02f5da
Paul SUPIOT <psupiot44@gmail.com>
2024-04-16 21:15:20 +0200

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,33 @@
 #include <DHT_U.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <time.h>
 
-#define DHTPIN 21
-#define DHTTYPE DHT11
+#define DHTPIN 21 
+#define DHTTYPE DHT11 
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(DHTPIN, DHTTYPE); 
 
-uint32_t delayMS;
+uint32_t delayMS; 
 
-const char* ssid = "SSID";
-const char* password = "PASSWORD";
-const char* url = "https://hook.eu2.make.com/id";
+const char* ssid = "iPhone 13 Paul";
+const char* password = "haricot9";
+const char* url = "https://hook.eu2.make.com/ggtizg0u4y2el4bzaw9lc5wr68mf93e5";
 
-unsigned long previousMillis = 0;
-const long interval = 600000; // 30 minutes
+unsigned long previousMillis = 0; 
+const long interval = 30000; 
 
-void sendData(float temperature, float humidity);
+struct Data {
+  float temperature;
+  float humidity;
+  float difference;
+};
+
+Data storedData[48];
+int dataIndex = 0;
+
+void recordData(float temperature, float humidity);
+void sendData();
 
 void setup() {
   Serial.begin(9600);
@@ -43,49 +54,73 @@ void loop() {
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+
     sensors_event_t temp_event, humidity_event;
     dht.temperature().getEvent(&temp_event);
-    if (isnan(temp_event.temperature)) {
-      Serial.println(F("Error reading temperature!"));
-    }
-    else {
-      Serial.print(F("Temperature: "));
-      Serial.print(temp_event.temperature);
-      Serial.println(F("°C"));
-    }
     dht.humidity().getEvent(&humidity_event);
-    if (isnan(humidity_event.relative_humidity)) {
-      Serial.println(F("Error reading humidity!"));
-    }
-    else {
-      Serial.print(F("Humidity: "));
-      Serial.print(humidity_event.relative_humidity);
-      Serial.println(F("%"));
-      sendData(temp_event.temperature, humidity_event.relative_humidity); // Envoyer les données à Google Sheets
+
+    if(!isnan(temp_event.temperature) && !isnan(humidity_event.relative_humidity)){
+      recordData(temp_event.temperature, humidity_event.relative_humidity);
+      sendData();
     }
   }
 }
 
-void sendData(float temperature, float humidity) {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+void recordData(float temp, float humi) {
+  storedData[dataIndex].temperature = temp;
+  storedData[dataIndex].humidity = humi;
+  storedData[dataIndex].difference = 0;
+  if (dataIndex > 0) {
+    for (int i = 0; i < dataIndex; i++) {
+      storedData[i].difference += 30;
+    }
   }
-  Serial.println("Connected to WiFi");
-
-  HTTPClient http;
-  String urlWithParams = String(url) + "?temp=" + String(temperature, 2) + "&humi=" + String(humidity, 2);
-  http.begin(urlWithParams);
-
-  int httpResponseCode = http.GET();
-  if(httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-  } else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
+  dataIndex++;
+  if (dataIndex == 48) {
+    dataIndex = 47;
   }
+  Serial.print("Index des données: ");
+  Serial.print(dataIndex);
+  Serial.print("Différence de l'index: ");
+  Serial.println(storedData[dataIndex].difference);
+  Serial.print(F("Temperature: "));
+  Serial.print(temp);
+  Serial.println(F("°C"));
+  Serial.print(F("Humidity: "));
+  Serial.print(humi);
+  Serial.println(F("%"));
+}
 
-  http.end();
+void sendData(){
+  if (WiFi.status() != WL_CONNECTED){
+    int tries = 0;
+    while (WiFi.status() != WL_CONNECTED && tries < 10) {
+      Serial.println("Tentative de connexion au réseau WiFi...");
+      WiFi.begin(ssid, password);
+      delay(1000);
+      tries++;
+    }
+  }
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    for (int i = 0; i < dataIndex; i++) {
+      float temp = storedData[i].temperature;
+      float humi = storedData[i].humidity;
+      float diff = storedData[i].difference;
+      String urlWithParams = String(url) + "?temp=" + String(temp, 2) + "&humi=" + String(humi, 2) + "&diff=" + String(diff, 2);
+      http.begin(urlWithParams);
+      Serial.println(urlWithParams);
+      int httpResponseCode = http.GET(); 
+      if(httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        http.end();
+      } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+        http.end(); 
+      }
+    }
+    dataIndex = 0;
+  }
 }
